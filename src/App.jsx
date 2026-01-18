699cbf9834444d74322e2606f47565018a680560
DiyaNavaz <diyanavasm@gmail.com>
2026-01-18 09:57:03 +0530

    Add alert log page with filters, table, and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./pages/Login";
-import Dashboard from './pages/Dashboard'
-import Alertlog from './pages/Alertlog'
-import DashboardLayout from './layouts/DashboardLayout'
+import Alertlog from "./pages/Alertlog";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route element={<DashboardLayout />} >
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/alerts" element={<Alertlog />} />
-      </Route>
-    </Routes>
-  );
+  return <Alertlog />;
 }
 
 export default App;
---
+++ src/data/alertData.js
@@ -0,0 +1,29 @@
+const alertData = [
+  {
+    id: "ALRT_001",
+    type: "Gunshot",
+    confidence: 94,
+    location: "9.364, 76.789",
+    time: "13/01/2026 1:36 PM",
+    status: "New"
+  },
+  {
+    id: "ALRT_002",
+    type: "Chainsaw",
+    confidence: 87,
+    location: "9.365, 76.790",
+    time: "13/01/2026 1:26 PM",
+    status: "Acknowledged"
+  },
+  {
+    id: "ALRT_003",
+    type: "Ambient",
+    confidence: 65,
+    location: "9.366, 76.791",
+    time: "13/01/2026 11:41 AM",
+    status: "Resolved"
+  }
+];
+
+export default alertData;
+
