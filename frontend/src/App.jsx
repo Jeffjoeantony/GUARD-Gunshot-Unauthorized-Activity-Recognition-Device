9763f8cfa6e30807e410d2c3d2ef1f0aede6ce98
Jeffjoeantony <antonyjeffjoe@gmail.com>
2026-04-21 07:56:42 +0530

    Removed current admin-dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Alertlog from "./pages/Alertlog";
-import AdminDashboard from "./pages/AdminDashboard";
 import MapPage from "./pages/MapPage";
 import DashboardLayout from "./layouts/DashboardLayout";
 import ResetPassword from "./pages/ResetPassword";
@@ -33,7 +32,6 @@ function App() {
 
         <Route path="/alerts" element={<ProtectedRoute> <Alertlog /> </ProtectedRoute> } />
 
-        <Route path="/admin" element={<AdminDashboard />} />
 
         <Route path="/map" element={<ProtectedRoute> <MapPage /> </ProtectedRoute>} />
 
